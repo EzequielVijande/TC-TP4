f056eab594104dc6c8ffefd9ff701a30ba18f78a
Ezequiel Vijande <evijande@itba.edu.ar>
2018-10-04 23:31:39 -0300

    Empece el repositorio

--- TC-TP4/TC_TP4.py
+++ TC-TP4/TC_TP4.py
@@ -1,1 +1,17 @@
-
+#Trabajo practico del Instituto Tecnologico de Buenos Aires
+#Teoria de circuitos- Diseño de filtros analogicos
+#Autores:
+#   Matias Larroque                 Legajo:56597
+#   Lucero Guadalupe Fernanadez     Legajo:57485
+#   Manuel Mollon                   Legajo:58023
+#   Ezequiel Vijande                Legajo:58057
+#
+#
+#Fecha de entrega:25/10/2018
+#
+#
+#   El siguiente programa consiste en la realizacion de una interfaz grafica que permite que un usuario ingrese
+# los parametros necesarios para realizar la plantilla de un filtro analogico. La interfaz grafica permite
+# la eleccion del tipo de filtro(Pasa-Bajos, Pasa-Altos, Pasa-Banda,Rechaza-Banda) asi como tambien la aproximacion
+# que el usuario desea emplear(Algunas opciones son Butterworth y Chebycheff). Luego de seleccionar el tipo de filtro y
+# aproximacion deseada, se deben establecer los parametros de la plantilla que se desea.
